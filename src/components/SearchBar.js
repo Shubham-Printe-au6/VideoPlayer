58c60cf80d8811b21084bb76d4e63b17f4834c7c
Shubham Printe <shubham.printe@dogratech.com>
2021-10-05 23:14:55 +0530

    finished youtube videos project using class components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+    state = {term: ''};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onFormSubmit(this.state.term);
+    }
+
+    render() {
+        return( 
+            <div className='search-bar ui segment'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
+                    <div className='field'>
+                        <label>Video Search</label>
+                        <input 
+                        value={this.state.term} 
+                        onChange={(event) => this.setState({term: event.target.value})} 
+                        type='text' 
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
